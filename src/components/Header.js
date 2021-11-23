be5b330f7705b2ade4a1d9ecba898e3f928d5b36
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-23 13:12:34 -0700

    Header Styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,13 @@ import './Header.css'
 export default class Header extends Component {
     render() {
         return (
-           <div className="header">
-               <span className="header_text">Hello world!</span>
+           <div div className="header">
+               <a 
+                 className="home_link" 
+                 href="http://localhost:3001/">
+                 
+                 Home
+               </a>
            </div> 
         )
     }
