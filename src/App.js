7001a82d1bd374ab641332f5bac478fce3fae7ed
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-24 13:13:22 -0700

    Display Populating

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 //Saved for reference
 import "./App.css";
 
 //Components
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header";
 import Display from "./components/Display/display";
-import Nav from "./components/Nav/nav"
+import Nav from "./components/Nav/nav";
+import data from "./data";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
-      
-    }
-  }
-  render () {
-    return (
-    <div className="App_Parent">
-      <Header />
-      <Display />
-      <Nav />
+      employees: [data[0]],
 
-    </div>
-    )
+      /*  
+      id: null,
+      name: {first: "", last: ""}, 
+      city: "",
+      country: "",
+      employer: "",
+      favMovies: [
+        "",
+        "",
+        ""
+      ]
+    */
+    };
+  }
+
+  nextEmployee() {
+    //this.setState(employee: data[1])
+  }
+
+  render() {
+    const {employees} = this.state
+    return (
+      <div className="App_Parent">
+        <Header />
+
+        {employees.map((employee) => (
+          <Display
+            id={employee.id}
+            name={employee.name}
+            city={employee.city}
+            country={employee.country}
+            employer={employee.employer}
+            title={employee.title}
+            favMovies={[employee.favoriteMovies]}
+          />
+        ))}
+
+        <Nav />
+      </div>
+    );
   }
 }
 
--- src/components/Display/display.js
+++ src/components/Display/display.js
@@ -4,23 +4,26 @@ import "./display.css";
 
 export default class Display extends Component {
   render() {
+    const {id, name, city, country, employer, title, favMovies} = this.props
+
+
     return (
       <div className="Display_Box">
-        <div className="Employee_Number">1/25</div>
+        <div className="Employee_Number">{id}/25</div>
         <div className="Employee_Info_Box">
-          <h2 className="Employee_Name">Name Big</h2>
+          <h2 className="Employee_Name">Name</h2>
           <ul className="Employee_Info_List">
-            <li className="Employee_List_Item">From:</li>
-            <li className="Employee_List_Item">Job Title:</li>
-            <li className="Employee_List_Item">Employer:</li>
+            <li className="Employee_List_Item">From: <span className="Employee_Array_Text">{city}, {country}</span></li>
+            <li className="Employee_List_Item">Job Title: <span className="Employee_Array_Text">{title}</span></li>
+            <li className="Employee_List_Item">Employer: <span className="Employee_Array_Text">{employer}</span></li>
           </ul>
         </div>
         <div className="Fav_Movies_Box">
           <span className="Fav_Movies_Title">Favorite Movies:</span>
           <ol className="Movies_List">
-            <li className="Movies_List_Item">Fathom</li>
-            <li className="Movies_List_Item">Strange Magic</li>
-            <li className="Movies_List_Item">Martian Child</li>
+            <li className="Movies_List_Item">{favMovies[0]}</li>
+            <li className="Movies_List_Item">{favMovies}</li>
+            <li className="Movies_List_Item">{favMovies}</li>
           </ol>
         </div>
       </div>
