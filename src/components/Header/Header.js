e40e49263e2634414a1403ba968740c73d4855f7
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2021-11-23 19:03:53 -0700

    Display files added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div div className="header">
+        <a className="home_link" href="http://localhost:3001/">
+          Home
+        </a>
+        
+      </div>
+    );
+  }
+}
